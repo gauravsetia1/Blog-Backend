083c265d030739fc2fab31387c376c669042488c
Gaurav Setia <gauravsetia1999@gmail.com>
2019-11-17 15:17:21 +0530

    Comment feature added

--- src/main/java/com/chitkara/blog/controller/CommentController.java
+++ src/main/java/com/chitkara/blog/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.chitkara.blog.controller;
 
+import com.chitkara.blog.model.Blog;
 import com.chitkara.blog.model.Comment;
+import com.chitkara.blog.repository.BlogRepo;
 import com.chitkara.blog.repository.CommentRepo;
 import com.chitkara.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comment")
@@ -21,6 +24,9 @@ public class CommentController {
     @Autowired
     CommentRepo commentRepo;
 
+    @Autowired
+    BlogRepo blogRepo;
+
     @PostMapping("/add/{id}")
     public String addBlog(@PathVariable(value="id")Long id, Principal principal, @Valid @RequestBody Comment comment) {
         String str = comment.getComment();
@@ -30,5 +36,11 @@ public class CommentController {
     @GetMapping("/get")
     public List<Comment> getComment() {
         return commentRepo.findAll();
+    }
+
+    @GetMapping("/getCmt/{id}")
+    public List<Comment> getCmt(@PathVariable(value="id")Long id) {
+        Optional<Blog> blog = blogRepo.findById(id);
+        return commentRepo.findAllByBlog(blog);
     }
 }
--- src/main/java/com/chitkara/blog/repository/CommentRepo.java
+++ src/main/java/com/chitkara/blog/repository/CommentRepo.java
@@ -1,10 +1,15 @@
 package com.chitkara.blog.repository;
 
+import com.chitkara.blog.model.Blog;
 import com.chitkara.blog.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepo extends JpaRepository<Comment , Long> {
 
+    public List<Comment> findAllByBlog(Optional<Blog> blog);
 }
